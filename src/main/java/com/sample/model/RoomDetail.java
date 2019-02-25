e8cbb7802ecce5f5af230b586d1c22df311d9a12
fscyhcyh <32520004+fscyhcyh@users.noreply.github.com>
2019-02-24 23:58:11 -0800

    Create RoomDetail.java

---
+++ src/main/java/com/sample/model/RoomDetail.java
@@ -0,0 +1,29 @@
+package com.sample.model;
+
+public class RoomDetail {
+	
+	private String type;
+	private int rooms;
+	private int guests;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int getRooms() {
+		return rooms;
+	}
+	public void setRooms(int rooms) {
+		this.rooms = rooms;
+	}
+	public int getGuests() {
+		return guests;
+	}
+	public void setGuests(int guests) {
+		this.guests = guests;
+	}
+	
+
+}
